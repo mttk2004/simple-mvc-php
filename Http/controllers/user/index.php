bfac219b372ce8a66686cd93c49f3305ef271048
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2024-12-11 21:51:26 +0700

    v0.2.8: [new: twig readme]

--- Http/controllers/user/index.php
+++ Http/controllers/user/index.php
@@ -7,12 +7,8 @@ $db = resolveDatabase();
 $userData = $db->query('SELECT * FROM users')->findAll();
 $users = [];
 
-
 foreach ($userData as $data) {
     $users[] = new User($data['id'], $data['name'], $data['email'], $data['password']);
 }
 
-//view('users/index', ['users' => $users]);
-$twig = require_once BASE_PATH . 'config/twig.php';
-// dd($twig);
-echo $twig->render('users/index.html.twig', ['users' => $userData]);
+view('users/index', ['users' => $users]);
