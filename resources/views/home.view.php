587418603ea2dd7f9d7c044e889af0543f403121
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2024-12-11 15:34:34 +0700

    v0.2.6: [new: tailwindcss]

--- resources/views/home.view.php
+++ resources/views/home.view.php
@@ -7,8 +7,16 @@
 			content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Document</title>
+
+	<link rel="stylesheet" href="/style.css">
 </head>
 <body>
-	Home
+	<h1 class="text-3xl text-blue-500 font-bold underline">
+		Hello world!
+	</h1>
+
+	<p class="text-red-500">
+		Hello world!
+	</p>
 </body>
 </html>
