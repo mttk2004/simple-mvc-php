34f8108b5f8d28fade5b202a2315d01af7170f1d
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2024-12-15 09:45:36 +0700

    update(config): remove useless code in twig.php

--- config/twig.php
+++ config/twig.php
@@ -3,8 +3,6 @@
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 
-require_once BASE_PATH . 'vendor/autoload.php';
-
 $loader = new FilesystemLoader(BASE_PATH . 'resources/views');
 $twig = new Environment($loader, [
         'cache' => BASE_PATH . 'storage/cache/twig',
